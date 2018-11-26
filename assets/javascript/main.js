521e0d3c1b3cbe6fe5eceb672ec3776a08e0785b
Kyle Kratt <kylekratt@gmail.com>
2018-11-26 00:00:57 -0500

    Added basic page layout with minimal formatting

---
+++ assets/javascript/main.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDcyyrkMbj4iyHCYEknSz2g1lip1-xo4eQ",
+    authDomain: "rock-paper-scissors-37332.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-37332.firebaseio.com",
+    projectId: "rock-paper-scissors-37332",
+    storageBucket: "",
+    messagingSenderId: "495755726939"
+  };
+  firebase.initializeApp(config);
