e988b14ed686201ee2337211966c7e1b890076f8
Kyle Kratt <kylekratt@gmail.com>
2018-11-28 23:30:31 -0500

    Removed obsolete code, fixed the youtube reference, added comments for easier reading

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -19,10 +19,12 @@ var ties=0;
 var losses=0;
 
 database.ref().on("value", function (snapshot) {
+  //This sets the values for whether player 1 and player 2 are picked to false, to be disproved
   p1 = false;
   p2 = false;
   $("#p1choice,#p2choice").attr("disabled", false);
   $("#p1name,#p2name").text("");
+  //This checks each currently connected user to see whether player 1 and player 2 are currently picked, and displays their information if they are.
   Object.keys(snapshot.val().connections).forEach(function (element) {
     var snapval = snapshot.val().connections[element];
     if (snapval.pchoice == "player1") {
@@ -44,17 +46,17 @@ database.ref().on("value", function (snapshot) {
       $("#2losses").text(p2ref.losses);
     }
   })
-  database.ref().update({
-    player1: p1,
-    player2: p2
-  })
+  //This displays the arena contents or hides them depending on whether both player slots are filled
   if (p1 && p2) {
     start();
   }
   else
     hold();
 })
+//This activates whenever a line is entered in the chatbox
 stringRef.on("value", function(snapshot){
+  //Only users who are logged in as a player or a spectator can see the chat
+  //If they are, the string is appended to the chat history, along with the time that the string was written
   if (player!=null){
     var newDiv = $("<div>");
     newDiv.css({"margin": "5px"}).text(snapshot.val().str);
@@ -65,12 +67,14 @@ stringRef.on("value", function(snapshot){
     $("#history").scrollTop($("#history").prop("scrollHeight"));
   }
 })
+//This keeps track of your current connection 
 connectedRef.on("value", function (snap) {
   if (snap.val()) {
     con = connectionsRef.push(true);
     con.onDisconnect().remove();
   }
 });
+//Once you login with a player selection and a name choice, your database information is updated
 $("#submit").on("click", function (event) {
   event.preventDefault();
   if ($("#pchoice").val() == null)
@@ -81,17 +85,19 @@ $("#submit").on("click", function (event) {
     throw: false,
     pchoice: player,
     pname: pname,
-    wins: 0,
-    ties: 0,
-    losses: 0
+    wins: wins,
+    ties: ties,
+    losses: losses
   })
 })
+//This allows you to throw rock paper or scissors
 $(".hand").on("click", function(event){
   con.update({
     throw: true,
     throwType: $(this).attr("id")
   })
 })
+//This displays the arena when both player slots are taken. The rock paper scissors buttons are only displayed for the two players, not spectators.
 function start() {
   $("#arena").css("visibility", "visible");
   if ((player != "player1") && (player != "player2")) {
@@ -99,6 +105,7 @@ function start() {
   }
   else
     $(".hand").css("visibility", "visible");
+//This shows the outcome once both players have made a pick. It returns the throw value back to false so the game knows not to run this again until both players have picked again.
   if ((p1ref.throw==true)&&(p2ref.throw==true)){
     con.update({
       throw: false
@@ -106,9 +113,11 @@ function start() {
     combat();
   }
 }
+//This hides the arena shown by the start function, in the event that a player leaves.
 function hold() {
   $("#arena,.hand").css("visibility", "hidden")
 }
+//This takes all the cases and displays the choice each player made in the arena
 function combat(){
   $("#gameres").css("opacity", "0");
   if (p1ref.throwType=="rock"){
@@ -129,6 +138,7 @@ function combat(){
   else if (p2ref.throwType=="scissors"){
     $("#p2throwimg").attr("src", "assets/images/scissors.png")
   }
+  //This animates those choices to make them look cooler
   $("#p1throw, #p2throw").css("opacity","1");
   $("#p1throw").animate({left: '100px'}, "fast");
   $("#p1throw").animate({left: '0px'}, "fast");
@@ -138,6 +148,7 @@ function combat(){
   $("#p2throw").animate({right: '0px'}, "fast");
   $("#p2throw").animate({right: '30px'}, "slow");
   $("#p2throw").animate({opacity: "0"}, 500);
+  //This takes all the cases for the possibility of a tie, win, or loss, updates their database reference, and displays the results in the gameres div.
   if ((p1ref.throwType=="rock")&&(p2ref.throwType=="rock")||(p1ref.throwType=="paper")&&(p2ref.throwType=="paper")||(p1ref.throwType=="scissors")&&(p2ref.throwType=="scissors")){
     if (player=="player1"){
       ties++;
@@ -183,12 +194,18 @@ function combat(){
     }
     $("#gameres").text("Player 2 Wins!");
   }
+  //This displays the results in a stylish fashion.
   setTimeout(function(){$("#gameres").animate({"opacity": "1"}, 1200)},800);
 }
+//This runs when the user presses enter in the chatbox
 function upload(){
   if(event.key === 'Enter'){
     event.preventDefault();
+    //This updates the latest input and the time it was put in. Only one string is in the database at a time because I'm cheap.
     if (player!=null){
+      if ($("#textbox").val()==""){
+        return;
+      }
       var newStr = pname + ": " + $("#textbox").val();
       $("#textbox").val("");
       stringRef.update({
@@ -196,6 +213,7 @@ function upload(){
         time: (moment().format("DD-MM-YY HH:mm:ss"))
       })
     }
+//This prevents users who aren't logged in from using the chatbox and shows them the error of their ways.
     else{
       var newDiv = $("<div>");
       newDiv.css({"margin": "5px"}).text("Please login before using the chat");
